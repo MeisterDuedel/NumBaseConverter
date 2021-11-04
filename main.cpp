f8417e8333d7cdb9a7df0bb2ef86bef8e25d3a07
Christoph Pircher <christoph@christoph-pircher.de>
2021-11-04 12:10:07 +0100

    Added comment to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,11 @@
+/*
+    A short program to convert numbers between bases (base 2 to 62) for educational purposes.
+    Author: Christoph Pircher
+    Created: 28.10.2021 (DD-MM-YYYY)
+    Last edited: 04.11.2021
+    Licenced under GPL-V3
+*/
+
 #include "mainwindow.h"
 #include "functions.h"
 
